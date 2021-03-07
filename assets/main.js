dd24d2ca5e0ef51609bbe1e4622da129a533073e
Sarah B <sarahbinaz1020@gmail>
2021-03-07 15:00:31 -0700

    added html and js

---
+++ assets/main.js
@@ -0,0 +1,24 @@
+// Setting time at the top of the page
+
+// global variable for 
+var currentDay = document.getElementById("#currentDay");
+
+// Global variable date
+var date = document.getElementById("#date");
+
+// Set date format
+var date = (moment().format('dddd Do YYY, h:mm a'));
+
+// show date on page
+document.getElementById("currentDay").innerHTML = date;
+
+// Set interval on time to update every 1 minute
+var timeUpdate = setInterval (date, 60000);
+
+function date() {
+    var d = new Date();
+    var t = d.toLocaleTimeString();
+    document.getElementById("demo").innerHTML = t;
+}
+
+
