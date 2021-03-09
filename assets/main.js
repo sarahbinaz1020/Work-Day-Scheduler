e13df962625d407520fc1436739d4da04327c60b
Sarah B <sarahbinaz1020@gmail>
2021-03-08 19:21:03 -0700

    JS added

--- assets/main.js
+++ assets/main.js
@@ -7,7 +7,7 @@ var currentDay = document.getElementById("#currentDay");
 var date = document.getElementById("#date");
 
 // Set date format
-var date = (moment().format('dddd Do YYY, H:mm a'));
+var date = (moment().format('dddd Do YYY, h:mm a'));
 
 // show date on page
 document.getElementById("currentDay").innerHTML = date;
@@ -21,37 +21,46 @@ function date() {
     document.getElementById("demo").innerHTML = t;
 }
 
-// Setting up the scheduler
-// var hourEl = $('.hour');
-
-// var descriptionEl = $('.description');
-// console.log(descriptionEl);
-// color code the time blocks - text area - past/current/future time colors
-
+// global variable for the current 
 var currentHour = moment().format("H");
-console.log(currentHour);
-
 
 // Identify each hour block in textarea
 $(".description").each(function(index) {
-    console.log(index);
-    console.log($(this).parent().attr('id'));
+    // take off hour- in row time-block id
     var hour = $(this).parent().attr('id').split("-")[1];
+    // compare if current hour is greater than div class id hour
     if (parseInt(hour) > parseInt(currentHour) ){
+        // code to future css color
         $(this).addClass("future");
+        // compare if current hour equals div class id hour (green)
     } else if (parseInt(hour) === parseInt(currentHour) ) {
+        //  code to present css color (red)
         $(this).addClass("present");
     } else {
+        // else make past css color (gray)
         $(this).addClass("past");
     }
-
 })
-
 
 // Set-up text input boxes to save info with page refresh 
 
-    // set-up click on save button
+// Pull the hour for local storage
+$(".time-block").each(function() {
+    // Get elements by id
+    const id = $(this).attr("id");
 
-    // save text input data with page refresh-capturing text
+    // Retrive data from local storage
+    var storedDescription = localStorage.getItem(id);
 
-    // set-up local storage to save 
+    $(this).children("textarea").val(storedDescription);
+
+})
+// click on button and log input to local storage
+$(".saveBtn").on("click", function () {
+    // create variable and DOM traverse to parent of save button to the id identifier
+    var id = $(this).parent().attr("id");
+    // get the value from the input area
+    var description = $(this).siblings("textarea").val();
+    // store value from input area into local storage
+    localStorage.setItem(id, description);
+})
