a6b175f845baa19e3d88a68f5c4a264219a3b119
Sarah B <sarahbinaz1020@gmail>
2021-03-08 18:00:18 -0700

    More JS added

--- assets/main.js
+++ assets/main.js
@@ -7,7 +7,7 @@ var currentDay = document.getElementById("#currentDay");
 var date = document.getElementById("#date");
 
 // Set date format
-var date = (moment().format('dddd Do YYY, h:mm a'));
+var date = (moment().format('dddd Do YYY, H:mm a'));
 
 // show date on page
 document.getElementById("currentDay").innerHTML = date;
@@ -22,7 +22,36 @@ function date() {
 }
 
 // Setting up the scheduler
+// var hourEl = $('.hour');
 
-var scheduler = new p.Calendar(document.getElementById("container"));
+// var descriptionEl = $('.description');
+// console.log(descriptionEl);
+// color code the time blocks - text area - past/current/future time colors
 
-scheduler.render();
+var currentHour = moment().format("H");
+console.log(currentHour);
+
+
+// Identify each hour block in textarea
+$(".description").each(function(index) {
+    console.log(index);
+    console.log($(this).parent().attr('id'));
+    var hour = $(this).parent().attr('id').split("-")[1];
+    if (parseInt(hour) > parseInt(currentHour) ){
+        $(this).addClass("future");
+    } else if (parseInt(hour) === parseInt(currentHour) ) {
+        $(this).addClass("present");
+    } else {
+        $(this).addClass("past");
+    }
+
+})
+
+
+// Set-up text input boxes to save info with page refresh 
+
+    // set-up click on save button
+
+    // save text input data with page refresh-capturing text
+
+    // set-up local storage to save 
